6da77fce2b9f4242bf506afe921be7851ce43836
student892 <relsqwe@mail.ru>
2021-01-18 00:56:38 +0300

    gendiff joining

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,9 +4,7 @@ import genDiff from '../index.js';
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
-
-program
+  .version('1.0.0')
   .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2, program.format)))
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import compareFiles from './compareFiles.js';
 import parsers from './parsers.js';
 import chooseFormat from './formatters/index.js';
 
-const absolutePath = (filepath) => path.resolve(filepath);
+const getFullPath = (filepath) => path.resolve(filepath);
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
-  const absolutePath1 = absolutePath(filepath1);
-  const absolutePath2 = absolutePath(filepath2);
-  const data1 = parsers(absolutePath1);
-  const data2 = parsers(absolutePath2);
+  const fullPath1 = getFullPath(filepath1);
+  const fullPath2 = getFullPath(filepath2);
+  const data1 = parsers(fullPath1);
+  const data2 = parsers(fullPath2);
 
   const diffs = compareFiles(data1, data2);
   const result = chooseFormat(format, diffs);
